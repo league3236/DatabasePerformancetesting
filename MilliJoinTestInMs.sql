5391529b3f6b387a03c0d397b96266aaf78d47a9
KwakSeungHyeok <league3236@gmail.com>
2018-05-01 12:38:54 +0900

    mssql에서조인.. 시간이 오래걸리는구만..

---
+++ MilliJoinTestInMs.sql
@@ -0,0 +1,3 @@
+SELECT GETDATE() 'Today', DATEPART(ms,GETDATE()) 'MilliSecond Part';
+select * from table1_100000 INNER JOIN table2_100000 ON table1_100000.T1a = table2_100000.T2a;
+SELECT GETDATE() 'Today', DATEPART(ms,GETDATE()) 'MilliSecond Part';
