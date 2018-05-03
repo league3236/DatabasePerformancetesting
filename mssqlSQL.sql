2690baa4ed2d0db7f8a8ab17aac1d550973bb84b
KwakSeungHyeok <league3236@gmail.com>
2018-05-03 19:38:34 +0900

    두번째 sql

---
+++ mssqlSQL.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO result(result) SELECT DATEPART(ms,GETDATE()) 'MilliSecond Part';
+select * from table200 where aa=75;
+INSERT INTO result(result) SELECT DATEPART(ms,GETDATE()) 'MilliSecond Part';
